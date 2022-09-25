dcbbdadbb8815a193116d0268041420b619ba6f3
juancgar <juangarza373@gmail.com>
2022-09-25 01:02:48 -0500

    devops

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ from typing import Union
 from typing import Dict
 from pydantic import BaseModel
 from fastapi import FastAPI
-from data_read import popula_flujo_neto
+from app.data_read import popula_flujo_neto
 
 app = FastAPI()
-
 
 class GraphBase(BaseModel):
 
@@ -14,11 +13,9 @@ class GraphBase(BaseModel):
     egresos: int
     sinIngresos: bool
     sinEgresos: bool
-
 @app.get("/")
 def home():
     return {"message":"Hello TutLinks.com"}
-    
 #regresa un arreglo de tuplas con su id y el valor neto ordenado de mayor a menor
 #parametro json y regresa un arreglo
 @app.post("/get-net-flux")
