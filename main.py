ade17030be41c0d0e7c34651988e3a19369fae68
juancgar <juangarza373@gmail.com>
2022-09-25 01:08:41 -0500

    Revert "devops"
    This reverts commit dcbbdadbb8815a193116d0268041420b619ba6f3.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+<<<<<<< HEAD
 
 app = FastAPI()
 
@@ -8,3 +9,29 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message":"Hello TutLinks.com"}
+=======
+from data_read import popula_flujo_neto
+
+app = FastAPI()
+
+
+class GraphBase(BaseModel):
+
+    nombre: str
+    ingresos: int
+    egresos: int
+    sinIngresos: bool
+    sinEgresos: bool
+
+@app.get("/")
+def home():
+    return {"message":"Hello TutLinks.com"}
+    
+#regresa un arreglo de tuplas con su id y el valor neto ordenado de mayor a menor
+#parametro json y regresa un arreglo
+@app.post("/get-net-flux")
+def get_net_flux(data:Dict[str,GraphBase]):
+    return popula_flujo_neto(data)
+    
+    
+>>>>>>> parent of dcbbdad (devops)
