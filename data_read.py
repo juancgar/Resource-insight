c01e6b0ae0b08d0fcb852cf5c4eaeead5c43cc6d
Jcarlos0828 <jose.carlos4013@gmail.com>
2022-09-24 21:00:49 -0500

    considered edge cases

--- data_read.py
+++ data_read.py
@@ -6,6 +6,8 @@ def calculo_total_neto(ingresos, egresos):  # Recibe un diccionario
 def popula_flujo_neto(grafo):
     net_values = []
     for nodo in grafo:
+        if nodo.sinIngresos or nodo.sinEgresos:
+            continue
         ingreso, egreso = calculo_total_neto(
             nodo.ingresos.values(), nodo.egresos.values())
         net_values.append((ingreso-egreso, nodo.id))
