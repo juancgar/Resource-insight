5dc3b4235093d401ad53721f76f99c34d2e42253
juancgar <juangarza373@gmail.com>
2022-09-25 01:04:08 -0500

    dev ops

--- main.py
+++ main.py
@@ -1,25 +1,10 @@
-from typing import Union
-from typing import Dict
-from pydantic import BaseModel
 from fastapi import FastAPI
-from app.data_read import popula_flujo_neto
 
 app = FastAPI()
 
-class GraphBase(BaseModel):
+#domain where this api is hosted for example : localhost:5000/docs to see swagger documentation automagically generated.
 
-    nombre: str
-    ingresos: int
-    egresos: int
-    sinIngresos: bool
-    sinEgresos: bool
+
 @app.get("/")
 def home():
     return {"message":"Hello TutLinks.com"}
-#regresa un arreglo de tuplas con su id y el valor neto ordenado de mayor a menor
-#parametro json y regresa un arreglo
-@app.post("/get-net-flux")
-def get_net_flux(data:Dict[str,GraphBase]):
-    return popula_flujo_neto(data)
-    
-    
